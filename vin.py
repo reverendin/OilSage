988f2fc355c3fd8b82836c6827e46957aa92d789
eric <miracleofmagick@gmail.com>
2018-10-16 17:56:01 -0400

    Added gui functionality to vin search

--- vin.py
+++ vin.py
@@ -6,27 +6,22 @@ def get_vin(vin_num):
   answer = False
   while answer == False:
     #vin = input("Please enter vin number: ")
-    if vin_num != 17:
-      print("Vin must contain 17 characters!")
-      #messagebox.showinfo("Title", "a Tk MessageBox")
+    if len(vin_num) != 17:
       return "short"
 
-    url = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/'+ vin +'?format=json'
+    url = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinValues/'+ vin_num +'?format=json'
     r = requests.get(url)
 
     test = r.json()
     if test['Results'][0]['Make'] == '':
       print('Vin does not exist. Please try again')
+      return "wrong_vin"
     else:
+      car = test['Results'][0]
+      make = car['Make']
+      model = car['Model']
+      year = car['ModelYear']
+      cylinders = car['EngineCylinders']
+      displacement = round(float(test['Results'][0]['DisplacementL']), 1)
+      return(make, model, year, cylinders, displacement)
       answer = True
-
-  car = test['Results'][0]
-  make = car['Make']
-  model = car['Model']
-  year = car['ModelYear']
-  cylinders = car['EngineCylinders']
-  displacement = round(float(test['Results'][0]['DisplacementL']), 1)
-  return(car, make, model, year, cylinders, displacement)
-
-#print(year, make.lower().capitalize(), model, cylinders, displacement)
-
